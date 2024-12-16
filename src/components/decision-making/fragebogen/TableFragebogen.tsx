362cf0a682913b956f43e3fcf7ae315d0e94fd35
Lukas Sponsel <lukassponsel5@gmail.com>
2024-12-16 11:52:01 +0100

    icon entfernt für deployment

--- src/components/decision-making/fragebogen/TableFragebogen.tsx
+++ src/components/decision-making/fragebogen/TableFragebogen.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
+
 
 type QuestionType = 'dropdown' | 'checkbox';
 
